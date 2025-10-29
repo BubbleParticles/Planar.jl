eb5b1ddba6f79993c96ab0cce8f56340d7ee4a52
defnlnotme <a9pbg0vj6@mozmail.com>
2025-10-29 05:23:46 +0100

    fix: update environment variables for precompilation
    - Replaced Binance API keys with Bitmex API keys in Dockerfile, GitHub workflows, and test scripts.
    - Updated documentation to reflect the change in API key usage.
    - Adjusted build script to check for Bitmex API keys instead of Binance.

--- scripts/build-sysimage.sh
+++ scripts/build-sysimage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ -n "$PLANAR_BINANCE_SANDBOX_APIKEY" ] || { echo "missing api keys, direnv not sourced?"; exit 1; }
+[ -n "$PLANAR_BITMEX_SANDBOX_APIKEY" ] || { echo "missing api keys, direnv not sourced?"; exit 1; }
 
 # FIXME: JULIA_NUM_THREADS set to 1 temporarily because PackageCompiler 2.2 hangs on julia 1.11
 podman build \
