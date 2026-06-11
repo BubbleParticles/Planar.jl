8bd9c4c1d4c703ffbea4de1610ef7a68bc7b91ab
defnlnotme <a9pbg0vj6@mozmail.com>
2026-06-11 17:55:08 +0000

    fix: wrap precompile workloads in try/catch to tolerate missing gateway
    Fetch: use :okx instead of :cryptocom (unsupported 1d timeframe),
    wrap workload in try/catch since getexchange! returns bare exchange
    with no timeframes during precompilation.
    
    Stubs: Zarr Blosc compression type instability (Julia 1.12)
    causes UInt8/Vector{UInt8} conversion error during event trace
    writing. Wrap entire precompile workload in try/catch.

--- Fetch/src/precompile.jl
+++ Fetch/src/precompile.jl
@@ -1,25 +1,22 @@
 using .Misc.Lang: PrecompileTools, @preset, @precomp
-using .Misc: @skipoffline
 
-if get(ENV, "CCXT_GATEWAY_DISABLE", "") != "true"
 @preset let
     pair = "BTC/USDT"
     using .Data: zinstance
     using Exchanges.ExchangeTypes: _closeall
     tmp_zi = zinstance(mktempdir())
-    @warn "Precompilation of the `Fetch` module does api calls!"
     try
-        let e = getexchange!(:cryptocom)
-            @precomp @skipoffline begin
-                fetch_ohlcv(e, "1d", [pair]; zi=tmp_zi, from=-100, to=-10)
-                fetch_candles(e, "1d", [pair]; from=-100, to=-10)
-            end
+        e = getexchange!(:okx)
+        @precomp begin
+            fetch_ohlcv(e, "1d", [pair]; zi=tmp_zi, from=-100, to=-10)
+            fetch_candles(e, "1d", [pair]; from=-100, to=-10)
         end
         _closeall()
+    catch e
+        @debug "Fetch precompile workload skipped: $e"
     finally
         if hasproperty(tmp_zi.store, :a)
             rm(tmp_zi.store.a)
         end
     end
-end
 end
