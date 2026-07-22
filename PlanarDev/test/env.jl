37719072922a1f8a7ce27494b39c509776bd6c03
defnlnotme <a9pbg0vj6@mozmail.com>
2026-07-22 02:03:09 +0000

    cleanup: remove unused check_gateway function

--- PlanarDev/test/env.jl
+++ PlanarDev/test/env.jl
@@ -31,12 +31,3 @@ push!(LOAD_PATH, dirname(PROJECT_PATH))
 FAILFAST = true
 
 const _INCLUDED_TEST_FILES = Set{String}()
-
-function gateway_available()
-    try
-        Planar.Ccxt.ping()
-        return true
-    catch
-        return false
-    end
-end
